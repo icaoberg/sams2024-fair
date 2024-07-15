7bb52792f1125ed9f32fe857ecf403ad8d890359
Shayla <smathuri@andrew.cmu.edu>
2024-07-15 18:48:42 +0000

    Placce Charts In A Row

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ st.write(authors)
 today = 'August 3, 2024'
 st.write(today)
 
-abstract = ''' 
+abstract = '''
 # Abstract 
 The Human BioMolecular Atlas Program (HuBMAP) aims to create a comprehensive 3D-map representation of the human body and improve data access while developing methods for tissue interrogation applicable to other studies. In its first phase, HuBMAP achieved significant milestones, including the development of critical resources, standardized protocols, innovative imaging and sequencing techniques, and a reliable data integration platform. These efforts have led to the creation of high-resolution molecular and cellular maps that are essential resources for biomedical research. Researchers are expanding the map from 2D to 3D environments, incorporating niche factors such as age and ethnicity. The core value of HuBMAP is to provide freely accessible data via its online portal. Future directions include investigating changes in individual cells and neighborhoods during healthy aging and diseases that will help develop better drugs, predict disease outcomes, and understand disease progression in clinical settings. The program adheres to the FAIR guiding principles for scientific data management and stewardship, ensuring findability, accessibility, interoperability, and reusability of data. We researched these properties of HuBMAP, along with whether it has rich metadata, identifiable titles, standardized communication protocols, and open access to metadata even if the data itself is no longer available.
 '''
@@ -81,12 +81,7 @@ st.write(text)
 number_of_datasets = len(df.index)
 text = f'There are {number_of_datasets} published datasets'
 st.write(text)
-columns = [
-       'organ', 'dataset_type', 'group_name', 'data_access_level']
-df2= df[columns]
-df2.rename(columns={"organ": "Organ", "dataset_type": "Dataset Type", "data_access_level": "Data Access Level", "group_name": "Group Name"}, inplace=True)
-st.write(df2)
-
+st.write(df)
 number_of_organs = None
 text = f'There are 55 3D organs, 1203 tissue blocks'
 st.write(text)
@@ -117,6 +112,7 @@ plt.title('Percentage of Datasets with Donor Metadata')
 st.pyplot(fig)
 
 text = '### Dataset types'
+import matplotlib.pyplot as plt
 
 # Count the occurrences of each data access level in the dataframe
 access_level_counts = df['group_name'].value_counts()
@@ -129,32 +125,34 @@ data_counts = df['has_contributors'].value_counts()
 colors=["#3d5a6c","#a4c4d7"]
 colors = ["#5b6255","#cadF9E"]
 
-fig, ax = plt.subplots(figsize=(3,3))
-wedges, texts, autotexts = ax.pie(data_counts,autopct='%1.1f%%',startangle=90, colors=colors, shadow= True)
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(6,6))
+
+wedges, texts, autotexts = ax1.pie(data_counts,autopct='%1.1f%%',startangle=90, colors=colors, shadow= True)
 
 autotexts[0].set_color('white') 
 autotexts[1].set_color('black') 
 
-ax.legend(wedges, data_counts.index, title="Contributors", loc="center left", bbox_to_anchor=(1, 0, 0.5, 1))
+ax1.legend(wedges, data_counts.index, title="Contributors", loc="center left", bbox_to_anchor=(1, 0, 0.5, 1))
 
-ax.axis('equal')  
-ax.set_title('Distribution of "has_contributors"')
-st.pyplot(fig)
+ax1.axis('equal')  
+ax1.set_title('Distribution of "has_contributors"')
+#st.pyplot(fig)
 
 # Counting the number of datasets with contacts
 data_counts = df['has_contacts'].value_counts()
 colors = ["#5b6255","#cadF9E"]
 colors=["#3d5a6c","#a4c4d7"]
 
-fig, ax = plt.subplots(figsize=(3,3))
-wedges, texts, autotexts = ax.pie(data_counts, autopct='%1.1f%%', startangle=90, colors=colors, shadow= True)
+#fig, ax = plt.subplots(figsize=(3,3))
+wedges, texts, autotexts = ax2.pie(data_counts, autopct='%1.1f%%', startangle=90, colors=colors, shadow= True)
 
 autotexts[0].set_color('white') 
 autotexts[1].set_color('black') 
 
-ax.legend(wedges, data_counts.index, title="Contacts", loc="center left", bbox_to_anchor=(1, 0, 0.5, 1))
-ax.axis('equal')  
-ax.set_title('Distribution of "has_contacts"')
+ax2.legend(wedges, data_counts.index, title="Contacts", loc="center left", bbox_to_anchor=(1, 0, 0.5, 1))
+ax2.axis('equal')  
+ax2.set_title('Distribution of "has_contacts"')
+plt.tight_layout()
 st.pyplot(fig)
 
 text = '### Group name Dataset'
@@ -192,34 +190,6 @@ plt.tight_layout()
 # Display the chart
 st.set_option('deprecation.showPyplotGlobalUse', False)
 st.pyplot()
-
-
-# Count the occurrences of each data access level in the dataframe
-access_counts = df['data_access_level'].value_counts()
-
-# Generate a list of colors - one for each bar
-colors = ['skyblue', 'coral', 'lightgreen']  
-
-# Start making a bar chart to visualize the data
-access_counts.plot(kind='bar', color=colors) 
-
-# Add a title to the top of the chart
-plt.title('Data Access Level Distribution')
-
-# Label the x-axis (horizontal axis)
-plt.xlabel('Data Access Level')
-
-# Label the y-axis (vertical axis)
-plt.ylabel('Count')
-
-# Rotate the labels on the x-axis to 45 degrees
-plt.xticks(rotation=45)
-
-# Adjust the layout to make sure everything fits without clipping
-plt.tight_layout()
-
-# Display the chart
-plt.show()
 
 
 references = '''
