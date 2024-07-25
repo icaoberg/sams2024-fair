7a41394bddfed734b79e44c58dbc4afc47987291
Ivan Cao-Berg <icaoberg@psc.edu>
2024-07-25 13:09:59 -0400

    - Refactor determine_type function for consistency and readability. - Refactor get_data function for better error handling. - Refactor code for better variable naming and organization. - Update comments for clarity and conciseness. - Improve formatting and spacing for better code readability.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 
+
 ## DO NOT MODIFY THIS BLOCK
 # Function to determine the type
 def determine_type(dataset_type: str) -> str:
-    if '[' in dataset_type and ']' in dataset_type:
-        return 'Derived'
+    if "[" in dataset_type and "]" in dataset_type:
+        return "Derived"
     else:
-        return 'Primary'
+        return "Primary"
+
 
 @st.cache_data
 def get_data() -> pd.DataFrame:
@@ -28,13 +30,17 @@ def get_data() -> pd.DataFrame:
         json_data = response.json()  # Convert the response to JSON format
 
         # Ensure 'data' key exists in the JSON
-        if 'data' in json_data:  # Check if the JSON contains the key 'data'
-            df = pd.DataFrame(json_data['data'])  # Create a DataFrame using the data under 'data' key
-            df = df[df['status']=='Published']
-            df['dataset_status'] = df['dataset_type'].apply(determine_type)
+        if "data" in json_data:  # Check if the JSON contains the key 'data'
+            df = pd.DataFrame(
+                json_data["data"]
+            )  # Create a DataFrame using the data under 'data' key
+            df = df[df["status"] == "Published"]
+            df["dataset_status"] = df["dataset_type"].apply(determine_type)
             print("Data successfully loaded.")  # Print a message indicating success
         else:
-            raise KeyError("'data' key not found in the JSON response")  # Raise an error if 'data' key is missing
+            raise KeyError(
+                "'data' key not found in the JSON response"
+            )  # Raise an error if 'data' key is missing
 
         return df  # Return the DataFrame with the data
     except (ValueError, KeyError) as e:  # Catch errors related to value or missing keys
@@ -44,11 +50,11 @@ def get_data() -> pd.DataFrame:
         print(f"Request failed: {e}")  # Print the error message
         return pd.DataFrame()  # Return an empty DataFrame if the request fails
 
+
 df = get_data()
 ## DO NOT MODIFY THIS BLOCK
 
 # Convert the dictionary into a DataFrame
-
 
 
 ## DO NOT MODIFY THIS BLOCK
@@ -73,7 +79,7 @@ The Human BioMolecular Atlas Program (HuBMAP) aims to create a comprehensive 3D-
 """
 st.write(abstract)
 
-intro = '''
+intro = """
 # Introduction
 The Human BioMolecular Atlas Program (HuBMAP) is an initiative that aims to create a comprehensive multi-scale spatial atlas of the healthy human body. HuBMAP aims to help biomedical researchers visualize how the cells in the human body influence our health and can also help others understand the way in which the human body functions. HuBMAP can only finalize its atlas with the help of data providers, data curators and other contributors. 
 
@@ -85,13 +91,13 @@ With all the data provided and curated, contributors then develop innovative too
 
 Through the seamless integration of work from data providers, contributors, and HIVE, HuBMAP strives to create a high-tech transformational atlas that fosters inventions of new discoveries in the field of biomedical research. 
 
-'''
+"""
 st.write(intro)
 
-method = '''
+method = """
 # Overview
 The Human Biomolecular Atlas Program(HuBMAP) funded by the National Institute of Health(NIH), provides spatial molecular data which serves as a way to completely map out the human body to promote intuitive and revolutionary research. However, in order for the research to be comprehensive and understandable by professionals and the public alike, it needs to be Findable, Accessible, Interoperable, and Reusable. The FAIR guidelines provide a path for the HuBMAP data to lessen unavoidable bias, and corrupt data. The purpose of the project by the Carnegie Mellon Summer Academy for Math and Science team, serves as a means to analyze some of the HuBMAP data, and report back on the level to which the data adheres to the first two FAIR principles(Findable and Accessible). 
-'''
+"""
 st.write(method)
 
 text = "## Published data"
@@ -103,61 +109,59 @@ st.write(text)
 
 # At a glance sentences
 number_of_datasets = len(df)
-answer = f'- The number of datasets are {number_of_datasets}.'
+answer = f"- The number of datasets are {number_of_datasets}."
 st.write(answer)
 
-access_level_protected = df['data_access_level'].value_counts()['protected']
-answer = f'- The number of datasets that are protected is {access_level_protected}.'
+access_level_protected = df["data_access_level"].value_counts()["protected"]
+answer = f"- The number of datasets that are protected is {access_level_protected}."
 st.write(answer)
 
-access_level_public = df['data_access_level'].value_counts()['public']
-answer = f'- The number of datasets that are public is {access_level_public}.'
+access_level_public = df["data_access_level"].value_counts()["public"]
+answer = f"- The number of datasets that are public is {access_level_public}."
 st.write(answer)
 
-dataset_status_derived = df['dataset_status'].value_counts()['Derived']
-answer = f'- The number of datasets with a derived status is {dataset_status_derived}.'
+dataset_status_derived = df["dataset_status"].value_counts()["Derived"]
+answer = f"- The number of datasets with a derived status is {dataset_status_derived}."
 st.write(answer)
 
-dataset_status_primary = df['dataset_status'].value_counts()['Primary']
-answer = f'- The number of datasets with a primary status is {dataset_status_primary}.'
+dataset_status_primary = df["dataset_status"].value_counts()["Primary"]
+answer = f"- The number of datasets with a primary status is {dataset_status_primary}."
 st.write(answer)
 
-dataset_types = df['dataset_type'].unique()
+dataset_types = df["dataset_type"].unique()
 number_of_dataset_types = len(dataset_types)
-answer = f'- The number of dataset types are {number_of_dataset_types}.'
+answer = f"- The number of dataset types are {number_of_dataset_types}."
 st.write(answer)
 
-organs = df['organ'].unique()
+organs = df["organ"].unique()
 number_of_organs = len(organs)
-answer = f'- The number of organ types are {number_of_organs}.'
+answer = f"- The number of organ types are {number_of_organs}."
 st.write(answer)
 
-donors = df['donor_hubmap_id'].unique()
+donors = df["donor_hubmap_id"].unique()
 number_of_donors = len(donors)
-answer = f'- The number of donors are {number_of_donors}.'
+answer = f"- The number of donors are {number_of_donors}."
 st.write(answer)
 
-groups = df['group_name'].unique()
+groups = df["group_name"].unique()
 number_of_groups = len(groups)
-answer = f'- The number of groups are {number_of_groups}.'
+answer = f"- The number of groups are {number_of_groups}."
 st.write(answer)
 
-wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(df['group_name'].value_counts())
+wordcloud = WordCloud(
+    width=800, height=400, background_color="white"
+).generate_from_frequencies(df["group_name"].value_counts())
 # Display the Word Cloud using Streamlit
-st.set_option('deprecation.showPyplotGlobalUse', False)  # Disable deprecated warning
+st.set_option("deprecation.showPyplotGlobalUse", False)  # Disable deprecated warning
 plt.figure(figsize=(10, 5))  # Set the figure size
-plt.imshow(wordcloud, interpolation='bilinear')
+plt.imshow(wordcloud, interpolation="bilinear")
 plt.axis("off")
 st.pyplot()  # Show the plot
-#At a a glance sentences (closed)
-
-
-
-
+# At a a glance sentences (closed)
 
 
 number_of_datasets = len(df.index)
-text = f'There are {number_of_datasets} published datasets'
+text = f"There are {number_of_datasets} published datasets"
 st.write(text)
 
 
@@ -168,9 +172,17 @@ st.write(text)
 text = "### Datasets"
 st.write(text)
 
-columns = ["organ", "dataset_type", "group_name", "created_timestamp", "data_access_level"]
+columns = [
+    "organ",
+    "dataset_type",
+    "group_name",
+    "created_timestamp",
+    "data_access_level",
+]
 df_display = df[columns]
-df_display["created_timestamp"] = df_display["created_timestamp"].apply(lambda time: pd.to_datetime(time, unit="ms").strftime('%m-%d-%Y'))
+df_display["created_timestamp"] = df_display["created_timestamp"].apply(
+    lambda time: pd.to_datetime(time, unit="ms").strftime("%m-%d-%Y")
+)
 df_display["data_access_level"] = df_display["data_access_level"].str.capitalize()
 df_display.rename(
     columns={
@@ -178,24 +190,24 @@ df_display.rename(
         "dataset_type": "Dataset Type",
         "created_timestamp": "Date Added",
         "group_name": "Group Name",
-        "data_access_level": "Data Access Level"  
+        "data_access_level": "Data Access Level",
     },
     inplace=True,
 )
 st.write(df_display)
 
-#Title for Graphs
+# Title for Graphs
 
 text = "# Graphs"
 st.write(text)
 
-#Creating columns for graphs
+# Creating columns for graphs
 
 col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
 
 
 # Count how many times each unique value appears in the 'data_access_level' column
-access_level_counts = df['has_data'].value_counts()
+access_level_counts = df["has_data"].value_counts()
 
 # Start making a donut chart
 fig, ax = plt.subplots()  # Create a blank space (figure) where the chart will be drawn
@@ -205,27 +217,31 @@ colors = ["#cadF9E"]
 # Plot a pie chart that will later become a donut chart
 wedges, texts, autotexts = ax.pie(
     access_level_counts,  # This is the data we're using — the counts of each access level
-    autopct='%1.1f%%',  # This makes sure that each piece of the pie shows its percentage like "25.0%"
+    autopct="%1.1f%%",  # This makes sure that each piece of the pie shows its percentage like "25.0%"
     startangle=90,  # This starts the first piece of the pie at the top of the circle
-    wedgeprops=dict(width=0.3),  # This makes the pie chart have a hole in the middle, turning it into a donut chart
-    colors=colors
+    wedgeprops=dict(
+        width=0.3
+    ),  # This makes the pie chart have a hole in the middle, turning it into a donut chart
+    colors=colors,
 )
 
 # Draw a white circle in the middle to make it look like a donut instead of a pie
 centre_circle = plt.Circle(
-    (0,0),  # This places the circle in the middle of the chart
+    (0, 0),  # This places the circle in the middle of the chart
     0.70,  # This sets the size of the white circle, making sure it's small enough to see the data around it but big enough to make a 'hole'
-    fc='white'  # 'fc' stands for fill color, which we're setting to white here
+    fc="white",  # 'fc' stands for fill color, which we're setting to white here
 )
 fig.gca().add_artist(centre_circle)  # This adds the white circle to our chart
 
 ax.legend(wedges, access_level_counts.index, title="Contributors", loc="center")
 
 # Make sure the chart is a perfect circle
-ax.axis('equal')  # This command makes sure the height and width are the same, keeping our donut round
+ax.axis(
+    "equal"
+)  # This command makes sure the height and width are the same, keeping our donut round
 
 # Add a title to the chart
-plt.title('Percentages of Dataset with Data')
+plt.title("Percentages of Dataset with Data")
 
 
 # Display the plot in Streamlit
@@ -234,35 +250,25 @@ with col1:
     st.pyplot()
 
 
-
-
 # Count how many times each boolean appears in the data
 data_counts = df["has_donor_metadata"].value_counts()
 
 # Plot pie chart using Streamlit
-fig, ax = plt.subplots(figsize=(3,3))
-wedges, texts, autotexts = ax.pie(data_counts,
-                                  autopct='%1.1f%%',
-                                  startangle=90, colors=["#cadF9E"])
-centre_circle = plt.Circle(
-    (0,0),  
-    0.70,  
-    fc='white'  
+fig, ax = plt.subplots(figsize=(3, 3))
+wedges, texts, autotexts = ax.pie(
+    data_counts, autopct="%1.1f%%", startangle=90, colors=["#cadF9E"]
 )
+centre_circle = plt.Circle((0, 0), 0.70, fc="white")
 fig.gca().add_artist(centre_circle)
 
 ax.legend(wedges, data_counts.index, title="Has Metadata", loc="center")
-ax.axis('equal')
-plt.title('Percentage of Datasets with Donor Metadata')
+ax.axis("equal")
+plt.title("Percentage of Datasets with Donor Metadata")
 
 # Display the plot in Streamlit
 plt.tight_layout()
 with col2:
     st.pyplot()
-
-
-
-
 
 
 # Count the occurrences of each data access level in the dataframe
@@ -305,9 +311,11 @@ data_counts = df["has_contacts"].value_counts()
 colors = ["#5b6255", "#cadF9E"]
 colors = ["#3d5a6c", "#a4c4d7"]
 
-fig, ax = plt.subplots(figsize=(6,6))
+fig, ax = plt.subplots(figsize=(6, 6))
 
-wedges, texts, autotexts = ax.pie(data_counts,autopct='%1.1f%%',startangle=90, colors=colors, shadow= True)
+wedges, texts, autotexts = ax.pie(
+    data_counts, autopct="%1.1f%%", startangle=90, colors=colors, shadow=True
+)
 
 # fig, ax = plt.subplots(figsize=(3,3))
 wedges, texts, autotexts = ax.pie(
@@ -325,8 +333,14 @@ ax.legend(
     bbox_to_anchor=(1, 0, 0.5, 1),
 )
 
-ax.legend(wedges, data_counts.index, title="Contributors", loc="center left", bbox_to_anchor=(1, 0, 0.5, 1))
-ax.axis('equal')  
+ax.legend(
+    wedges,
+    data_counts.index,
+    title="Contributors",
+    loc="center left",
+    bbox_to_anchor=(1, 0, 0.5, 1),
+)
+ax.axis("equal")
 ax.set_title('Distribution of "has_contributors"')
 
 ax.axis("equal")
@@ -339,25 +353,32 @@ with col4:
 
 
 # Counting the number of datasets with contributors
-data_counts = df['data_access_level'].value_counts()
-colors = ["#5b6255","#cadF9E"]
+data_counts = df["data_access_level"].value_counts()
+colors = ["#5b6255", "#cadF9E"]
 
-fig, ax = plt.subplots(figsize=(3,3))
-wedges, texts, autotexts = ax.pie(data_counts,autopct='%1.1f%%',startangle=90, colors=colors, shadow= True)
+fig, ax = plt.subplots(figsize=(3, 3))
+wedges, texts, autotexts = ax.pie(
+    data_counts, autopct="%1.1f%%", startangle=90, colors=colors, shadow=True
+)
 
-autotexts[0].set_color('white') 
-autotexts[1].set_color('black') 
+autotexts[0].set_color("white")
+autotexts[1].set_color("black")
 
-ax.legend(wedges, [s.capitalize() for s in data_counts.index], title="Access Level", loc="center left", bbox_to_anchor=(1, 0, 0.5, 1))
+ax.legend(
+    wedges,
+    [s.capitalize() for s in data_counts.index],
+    title="Access Level",
+    loc="center left",
+    bbox_to_anchor=(1, 0, 0.5, 1),
+)
 
-ax.axis('equal')  
-ax.set_title('Data Acess Level Distribution')
+ax.axis("equal")
+ax.set_title("Data Acess Level Distribution")
 
 # Display the plot in Streamlit
 plt.tight_layout()
 with col5:
     st.pyplot()
-
 
 
 # Count the occurrences of each data access level in the dataframe
@@ -438,17 +459,17 @@ st.markdown(f'<p style="text-align:right">{text}</p>', unsafe_allow_html=True)
 
 
 # Introduction paragraph for VR
-vrIntro = '''
+vrIntro = """
 # VR Introduction
 Recent advancements in virtual reality (VR) development have sparked interest in applying VR to biomedical research and practice. VR allows for dynamic exploration and enables viewers to enter visualizations from various viewpoints (Camp et al., 1998). It also facilitates the creation of detailed visualizations of intricate molecular structures and biomolecular systems (Chavent et al., 2011; Gill and West, 2014; Trellet et al., 2018; Wiebrands et al., 2018).
 When viewed in VR, the spatiality of organs and tissue blocks mapped to the Human Reference Atlas (HRA) can be explored in their true size, providing a perspective that surpasses traditional 2D user interfaces. Added 2D and 3D visualizations can then offer data-rich context. The HRA Organ Gallery, a VR application, allows users to explore 3D organ models of the HRA in their true scale, location, and spatial relation to each other. Currently, the HRA Organ Gallery features 55 3D reference organs, 1,203 mapped tissue blocks from 292 demographically diverse donors and 15 providers, linking to over 6,000 datasets. It also includes prototype visualizations of cell type distributions and 3D protein structures.
-'''
+"""
 st.write(vrIntro)
 
-text = '### Dataset types'
+text = "### Dataset types"
 st.write(text)
 
-references = '''
+references = """
 # References
 * Bueckle, A., Qing, C., Luley, S., Kumar, Y., Pandey, N., & Borner, K. (2023, April 10). The HRA Organ Gallery affords immersive superpowers for building and exploring the Human Reference Atlas with virtual reality. Frontiers, 3. https://www.frontiersin.org/journals/bioinformatics/articles/10.3389/fbinf.2023.1162723/full
 * Camp, J. J., Cameron, B. M., Blezek, D., and Robb, R. A. (1998). Virtual reality in medicine and biology.” Future Generation Computer Systems. Telemedical Inf. Soc. 14 (1), 91–108. https://www.sciencedirect.com/science/article/abs/pii/S0167739X98000235 
@@ -460,18 +481,18 @@ references = '''
 * Trellet, M. L., Férey, N., Flotyński, J., Baaden, M., and Bourdot, P. (2018). Semantics for an integrative and immersive pipeline combining visualization and analysis of molecular data. J. Integr. Bioinforma. 15 (2), 20180004. https://doi.org/10.1515/jib-2018-0004 
 * Wiebrands, M., Malajczuk, C. J., Woods, A. J., Rohl, A. L., and Mancera, R. L. (2018). Molecular dynamics visualization (MDV): Stereoscopic 3D display of biomolecular structure and interactions using the Unity game engine. J. Integr. Bioinforma. 15 (2), 20180010. https://doi.org/10.1515/jib-2018-0010 
 * Wilkinson, M. D., Sansone, S. A., Schultes, E., Doorn, P., Bonino da Silva Santos, L. O., & Dumontier, M. (2018). A design framework and exemplar metrics for FAIRness. Scientific data, 5, 180118. https://doi.org/10.1038/sdata.2018.118
-'''
+"""
 
 st.write(references)
 
-acknowledgements = '''
+acknowledgements = """
 # Acknowledgements
 This is a placeholder 
-'''
+"""
 st.write(acknowledgements)
 
-conclusion = '''
+conclusion = """
 # Conclusion
-'''
+"""
 
 st.write(conclusion)
